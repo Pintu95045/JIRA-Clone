9eb3b1b1578f8c30d7c66a9c7d1186bbdf2dcb95
Pintu Giri <mrpintugiri@gmail.com>
2024-08-28 00:29:24 +0530

    TaskCard created

--- script.js
+++ script.js
@@ -7,6 +7,39 @@
  */
 
 
-let AddNewTaskButton=document.getElementById("addTask");
-let planningDiv=document.getElementById("planning");
 
+//every time new card creation while cling on button
+
+const defultTaskCardMsg="Write Your Task Here"; 
+const blankText="";
+
+let AddNewTaskButton = document.getElementById("addTask");
+let planningDiv = document.getElementById("planning");
+
+AddNewTaskButton.addEventListener("click", () => {
+    let taskCard = document.createElement("div");
+    taskCard.setAttribute("class", "taskCard");
+    taskCard.setAttribute("contenteditable", true);
+    taskCard.innerHTML = defultTaskCardMsg;
+    // Append the new task card to the planningDiv
+    planningDiv.appendChild(taskCard);
+
+
+    //existing text should be clear when click on the text field
+
+    taskCard.addEventListener("click",(event) => {
+        let selectCard=event.target;
+        if(selectCard.innerText==defultTaskCardMsg){
+            selectCard.innerHTML=blankText;
+        }
+    });
+
+    //blank card should be removed
+    taskCard.addEventListener("blur",(event) =>{
+        let blurCard=event.target;
+        if(blurCard.innerHTML===blankText){
+            blurCard.remove();
+        }
+    });
+ 
+});
