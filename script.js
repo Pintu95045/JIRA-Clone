8f327effeea884d9889b70799fcf4e22a354f0cb
Pintu Giri <mrpintugiri@gmail.com>
2024-08-27 21:58:16 +0530

    HTML done

---
+++ script.js
@@ -0,0 +1,12 @@
+/**
+ * Logic required
+ * 
+ * 1. Make sure an new item is created and added to the planning section when the button clicked.
+ * 2. user can able to move it to inprogress or planning as per their prefrences
+ * 
+ */
+
+
+let AddNewTaskButton=document.getElementById("addTask");
+let planningDiv=document.getElementById("planning");
+
